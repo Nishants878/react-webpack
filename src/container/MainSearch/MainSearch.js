7c8754aea687c2778ed0856ddc61a94f8fde3a8d
Nishant Kumar <nishants878@gmail.com>
2021-08-10 17:01:35 +0530

    added select field

--- src/container/MainSearch/MainSearch.js
+++ src/container/MainSearch/MainSearch.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./_MainSearch.module.scss";
 export default function MainSearch() {
+  const data = [
+    { country: "USA", capital: "Washington DC" },
+    {
+      country: "India",
+      capital: "New Delhi",
+    },
+  ];
+  const [val, setVal] = useState("");
   return (
     <div className={classes.MainContainer}>
       Search
       <div>
         <Link to="/">To Todos</Link>
+        <select
+          defaultValue=""
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        >
+          {data.map((item) => (
+            <option value={item.capital}>{item.country}</option>
+          ))}
+        </select>
+        <input type="text" value={val} />
       </div>
     </div>
   );
